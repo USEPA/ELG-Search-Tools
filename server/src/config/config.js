9ca13ee5496d553d69c5d018e03fdf735c638717
MHayward <mark.hayward@erg.com>
2020-01-27 13:42:03 -0500

    database config update for cloud.goc environments (use VCAP_SERVICES)

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,20 +1,55 @@
+let isLocal = false;
+let isDevelopment = false;
+let isStaging = false;
+
+let db = {
+  database: "elg_search",
+  user: "postgres",
+  password: "postgres",
+  options: {
+    dialect: "postgres",
+    host: "localhost",
+    port: "5432"
+  }
+};
+
+if (process.env.NODE_ENV) {
+  isLocal = "local" === process.env.NODE_ENV.toLowerCase();
+  isDevelopment = "development" === process.env.NODE_ENV.toLowerCase();
+  isStaging = "staging" === process.env.NODE_ENV.toLowerCase();
+}
+
+if (isLocal) {
+  console.log("Since local, using a localhost Postgres database.");
+} else {
+  if (process.env.VCAP_SERVICES) {
+    console.log("Using VCAP_SERVICES Information to connect to Postgres.");
+    vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+    db = {
+      database: "elg_search",
+      user: vcap_services["aws-rds"][0].credentials.username,
+      password: vcap_services["aws-rds"][0].credentials.password,
+      options: {
+        dialect: "postgres",
+        host: vcap_services["aws-rds"][0].credentials.host,
+        port: vcap_services["aws-rds"][0].credentials.port
+      }
+    };
+  } else {
+    console.log(
+      "VCAP_SERVICES Information not found. Attempting connection to localhost..."
+    );
+  }
+}
+
 module.exports = {
   port: process.env.PORT || 3001,
-  db: {
-    database: process.env.DB_NAME || 'elg_search',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || 'postgres',
-    options: {
-      dialect: process.env.DIALECT || 'postgres',
-      host: process.env.HOST || 'localhost',
-      port: process.env.PORT || '5432',
-    },
-  },
+  db: db,
   authentication: {
-    jwtSecret: process.env.JWT_SECRET || 'secret',
+    jwtSecret: process.env.JWT_SECRET || "secret"
   },
-  service: process.env.SERVICE_PROVIDER || 'Gmail',
-  email: process.env.EMAIL || 'elg@gmail.com',
-  emailPassword: process.env.EMAIL_PASSWORD || 'password',
-  baseUrl: process.env.BASE_URL || 'http://localhost:8080',
+  service: process.env.SERVICE_PROVIDER || "Gmail",
+  email: process.env.EMAIL || "elg@gmail.com",
+  emailPassword: process.env.EMAIL_PASSWORD || "password",
+  baseUrl: process.env.BASE_URL || "http://localhost:8080"
 };
