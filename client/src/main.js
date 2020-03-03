c6ae1c9897289139cc9ec3a28ec6405b0d59308c
MHayward <mark.hayward@erg.com>
2020-03-03 12:55:21 -0500

    - updates to properly allow for both local builds/runs and deployments to cloud.gov environments

--- client/src/main.js
+++ client/src/main.js
@@ -9,7 +9,8 @@ import App from './App';
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
-Vue.axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:3001';
+Vue.axios.defaults.baseURL = window.location.origin.replace('localhost:8081', 'localhost:3001');
+
 // Need to set these headers to no-cache to fix IE11 issue where new requests are not sent
 Vue.axios.defaults.headers.common['Cache-Control'] = 'no-cache';
 Vue.axios.defaults.headers.common.Pragma = 'no-cache';
