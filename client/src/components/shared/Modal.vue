dea76148346e150cb99b07999c8829d9d0fd71b8
Devin <devin.galloway@erg.com>
2020-05-08 20:45:35 -0600

    Add prop for Modal header

--- client/src/components/shared/Modal.vue
+++ client/src/components/shared/Modal.vue
@@ -1,20 +1,54 @@
 <template>
   <div class="modal is-active">
     <div class="modal-background" @click="$emit('close')"></div>
-    <div class="modal-content" @keydown.native.esc="close" role="dialog" aria-modal="true">
-      <div class="box">
-        <button type="button" class="button is-text modal" @click="$emit('close')" aria-label="Close">
-          <span class="fa fa-times"></span>
-        </button>
+    <div class="modal-card" @keydown.native.esc="close" role="dialog" aria-modal="true">
+      <header class="modal-card-head">
+        <p class="modal-card-title">{{ title }}</p>
+        <button class="delete is-small" aria-label="close" @click="$emit('close')"></button>
+      </header>
+      <div class="modal-card-body">
         <slot />
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  props: {
+    title: {
+      required: false,
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
 .modal {
   z-index: 9999;
+}
+
+.modal-card {
+  border-radius: 6px;
+}
+
+.modal-card-head {
+  padding: 1rem;
+}
+
+.modal-card-title {
+  padding-bottom: 0;
+  font-size: 1.1rem;
+  font-weight: bold;
+}
+
+.delete {
+  background-color: rgba(10, 10, 10, 0.5);
+
+  &:hover {
+    background-color: rgba(10, 10, 10, 0.3);
+  }
 }
 
 .button.is-text.modal {
