e383569b72a7fc7c226d4cd9a1b99b334c9c5137
MHayward <mark.hayward@erg.com>
2021-03-29 21:19:21 -0400

    use s3 for storing copies of help and glossary

--- server/src/controllers/glossary.js
+++ server/src/controllers/glossary.js
@@ -1,62 +1,26 @@
 const utilities = require('./utilities');
 
-const https = require('https');
-
 const fs = require('fs');
 
 module.exports = {
   list(req, res) {
     try {
-      /****************************************************************
-       Is Glossary/Terminology Services authorization variable set?
-       ****************************************************************/
-      if (process.env.ELG_GLOSSARY_AUTH) {
-        console.log('ELG_GLOSSARY_AUTH environmental variable found, continuing.');
+      //get latest version of the glossary from file system (updated from TS or s3 on each server restart)
+      const path = require('path');
+      const fileName = path.join(__dirname, '../s3/glossary.json');
 
-        let glossaryOptions = {
-          host: 'etss.epa.gov',
-          port: 443,
-          path: '/synaptica_rest_services/api/vocabs/name/ELG%20Glossary/terms/full',
-          headers: {
-            authorization: 'basic ' + Buffer.from(process.env.ELG_GLOSSARY_AUTH).toString('base64')
-          }
-        }
-
-        https.get(glossaryOptions, glossaryResponse => {
-          let response = '';
-
-          glossaryResponse.on('data', data => {
-            response += data;
-          });
-
-          glossaryResponse.on('end', () => {
-            if (glossaryResponse.statusCode !== 200) {
-              console.log (JSON.parse(response));
-              response = '[]';
-              console.log('Non-200 returned from glossary web service: ' + glossaryResponse.statusCode);
-            } else {
-              console.log('Successful glossary request');
-            }
-            res.status(200).send(JSON.parse(response));
-          });
-
-          glossaryResponse.on('error', err => {
-            console.log('Error returned from glossary web service: ' + utilities.sanitizeError(err.message));
-            res.status(200).send(JSON.parse('[]'));
-          });
-        });
-      } else {
-        console.log('Glossary/Terminology Services authorization variable NOT set!');
-        res.status(200).send(JSON.parse('[]'));
-      }
-    } catch (err) {
+      const file = fs.createReadStream(fileName);
+      file.pipe(res);
+    }
+    catch (err) {
       return res.status(400).send('Error !' + utilities.sanitizeError(err.toString()));
     }
   },
   help (req, res) {
     try {
+      //get latest version of help pdf from file system (updated from s3 on each server restart)
       const path = require('path');
-      const fileName = path.join(__dirname, '..\\..\\api-docs\\ELG Database Users Guide.pdf');
+      const fileName = path.join(__dirname, '../s3/ELG Database Users Guide.pdf');
 
       const file = fs.createReadStream(fileName);
       const stat = fs.statSync(fileName);
