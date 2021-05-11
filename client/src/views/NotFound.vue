203de5b6f82fda49f1429eafa6870d66cd3f6113
Devin <devin.galloway@erg.com>
2021-05-11 15:51:21 -0600

    Add NotFound page

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="section">
+    <h2 class="is-size-3 has-text-weight-bold">Page Not Found</h2>
+    <br />
+    <p class="is-size-5">
+      The page you're looking for does not exist.
+    </p>
+    <p class="is-size-5">
+      <router-link to="/"><span class="fa fa-home"></span>Return Home</router-link>
+    </p>
+  </div>
+</template>
